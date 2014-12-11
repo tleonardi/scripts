bfe547b4bafa88e563e7e8711b0e4517febb4300
Tommaso Leonardi - farm <tl344@ebi.ac.uk>
2014-12-11 10:09:03 +0000

    ProfileSplitLabels fixed in case categories==1

--- dtCompare.R
+++ dtCompare.R
@@ -260,8 +260,17 @@ if(!is.na(title)){
 }
 
 # Faceting
-if(nrow(categories)==1 && length(labels)>=2 && !is.na(secondaryLabels)){
-	profilePlot <- profilePlot + facet_grid(~secLabel, scales=profScales)
+if(nrow(categories)==1 && length(labels)>=2){
+	if(is.na(secondaryLabels) && profileSplitLabels) {
+		profilePlot <- profilePlot + facet_grid(~Label, scales=profScales)
+	}
+	if(!is.na(secondaryLabels)){
+		if(profileSplitLabels){
+			profilePlot <- profilePlot + facet_grid(Label~secLabel, scales=profScales)
+		}else{
+			profilePlot <- profilePlot + facet_grid(~secLabel, scales=profScales)
+		}
+	}
 }
 
 if(nrow(categories)>1) {
@@ -269,7 +278,7 @@ if(nrow(categories)>1) {
 		profilePlot <- profilePlot + facet_grid(Category~., scales=profScales)
 	}
 	else if (length(labels)>=2 && is.na(secondaryLabels)) {
-		if(!is.na(profileSplitLabels)){
+		if(profileSplitLabels){
 			profilePlot <- profilePlot + facet_grid(Category~Label, scales=profScales)
 		}else{
 			profilePlot <- profilePlot + facet_grid(Category~., scales=profScales)
